491526e2312a7d00d9658a56934b62635ee2d53f
Anurag5 <anuraghere555@gmail.com>
2025-10-26 22:05:57 +0530

    Add files via upload

---
+++ 5.string.py
@@ -0,0 +1,19 @@
+# anything quoted inside " " and ' ' is called strings
+
+#IF we include anything in triple single quoute, all of the content becomes string
+
+luka = '''hi luka
+hi austin
+hi jared
+hi JJ'''
+
+print(luka)
+
+apple = 'he said, "i want to go to taj mahal"'
+
+name = "Anurag"
+
+#to run a loop
+
+for character in name:
+    print(character)
