ce867082fa9c30a4ba7c4cf8b8c2d8084a421393
Anurag5 <anuraghere555@gmail.com>
2025-10-29 21:21:06 +0530

    Add files via upload

---
+++ 8.ConditionalStatements.py
@@ -0,0 +1,11 @@
+a = int (input("Enter your age : "))
+print("your age is: ", a)
+
+if(a>18):
+  print("You can drive")
+elif(a==18):
+  print("You'll be able to drive vehicle in a year or so")  
+else:
+  print("You cannot drive")
+
+print("Investigation successfull")
