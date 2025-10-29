56d52f3a561ec40f2e9fc963d0bcc4126f35dbf1
Anurag5 <anuraghere555@gmail.com>
2025-10-29 21:22:10 +0530

    Add files via upload

---
+++ 13.function.py
@@ -0,0 +1,23 @@
+def calculateGmean(a,b):
+    mean =(a * b) / (a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+     print("first number is greater")
+    else:
+     print("second number is greater or equal")
+
+def isLesser(a,b):
+   pass #pass means i will write this function later(let me pass it for a while ,will come back to it in a sec)
+
+
+x = 4
+y = 5
+calculateGmean(x,y)
+isGreater(x,y)
+
+c =10
+d = 9
+calculateGmean(c,d)
+isGreater(c,d)
