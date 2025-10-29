e72eaa8a04f9f54048ba8ab37922abab94da3042
Anurag5 <anuraghere555@gmail.com>
2025-10-29 21:21:35 +0530

    Add files via upload

---
+++ 10.ForLoop.py
@@ -0,0 +1,23 @@
+name ="Anurag"
+for i in name:
+    print(i)
+    if(i == "a"):
+        print("Hi")
+
+colors = ["Red", "blue", "Green"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+for k in range(11):
+    print(k)
+
+for k in range(1,11):
+    print(k)
+
+print("Next stuff now")
+
+for k in range(1,12,3): #gives difference of 3
+    print(k)
+
