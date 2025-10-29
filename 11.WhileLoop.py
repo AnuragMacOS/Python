cbd48795544e4ff0ecff8dc627df111922658cbe
Anurag5 <anuraghere555@gmail.com>
2025-10-29 21:21:46 +0530

    Add files via upload

---
+++ 11.WhileLoop.py
@@ -0,0 +1,22 @@
+i = int(input("Enter the number: "))
+while(i<=38):
+    print(i)
+    i= i+1
+
+print('Done with the loop')
+
+
+count = 5
+while(count>0):
+    print(count)
+    count = count - 1
+
+else:
+    print("i am inside else rn")
+
+
+#do while loop doesnt work in python
+
+#do{
+    #loop body
+#}while(condition);
