5a4c5eece32b24c9ee0f68614fed9866b93c7884
Anurag5 <anuraghere555@gmail.com>
2025-10-31 23:51:43 +0530

    Add files via upload

---
+++ 20.Docstring & Pep-8.py
@@ -0,0 +1,12 @@
+def square(n):
+    '''Takes in a number n, returns the square of n'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+
+#---docstring are written right above function definition-----
+#comments are ignored by python
+
+
+#Pep-8 provides guidelines and best practices on how to write python code
