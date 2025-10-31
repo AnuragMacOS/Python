a9d8881b938aef2efd74fe35de66eb08eda7f37c
Anurag5 <anuraghere555@gmail.com>
2025-10-31 23:52:14 +0530

    Add files via upload

---
+++ 22.Sets.py
@@ -0,0 +1,8 @@
+# If we want a list with Unique characters only-->we use SETS
+# USE { } for sets
+# prints in random order
+
+s = {2,4,6,7,8,2}
+
+print(s) #-->prints -->{2, 4, 6, 7, 8} no repitition 
+
